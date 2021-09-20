574a21193559243f0081bb30eb07ff92ff4b1129
dimasm93 <software.dimas_m@icloud.com>
2021-09-20 16:17:57 +0700

    update config message

--- kafka-producer/src/main/java/com/tabeldata/message/producer/example/MainApplication.java
+++ kafka-producer/src/main/java/com/tabeldata/message/producer/example/MainApplication.java
@@ -2,7 +2,10 @@ package com.tabeldata.message.producer.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.kafka.support.converter.RecordMessageConverter;
+import org.springframework.kafka.support.converter.StringJsonMessageConverter;
 
 @EnableKafka
 @SpringBootApplication
@@ -10,5 +13,10 @@ public class MainApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(MainApplication.class, args);
+    }
+
+    @Bean
+    public RecordMessageConverter converter() {
+        return new StringJsonMessageConverter();
     }
 }
