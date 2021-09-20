635186e4f6c3aec2729436df71f7186b3803976c
dimasm93 <software.dimas_m@icloud.com>
2021-09-20 15:17:38 +0700

    enable kafka module

--- kafka-consumer/src/main/java/com/tabeldata/message/consumer/example/MainApplication.java
+++ kafka-consumer/src/main/java/com/tabeldata/message/consumer/example/MainApplication.java
@@ -2,7 +2,9 @@ package com.tabeldata.message.consumer.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.kafka.annotation.EnableKafka;
 
+@EnableKafka
 @SpringBootApplication
 public class MainApplication {
 
--- kafka-producer/src/main/java/com/tabeldata/message/producer/example/MainApplication.java
+++ kafka-producer/src/main/java/com/tabeldata/message/producer/example/MainApplication.java
@@ -2,7 +2,9 @@ package com.tabeldata.message.producer.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.kafka.annotation.EnableKafka;
 
+@EnableKafka
 @SpringBootApplication
 public class MainApplication {
 
